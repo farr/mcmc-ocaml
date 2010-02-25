9217732f214825514da806c8b8c2ee7cd75724ed
Will M. Farr <w-farr@northwestern.edu>
2010-02-25 13:08:11 -0600

    Re-named library to mcmc from denest.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -4,7 +4,7 @@ let oUnit_dir = "/Users/farr/Documents/code/oUnit"
 
 let _ = dispatch begin function 
   | After_rules -> 
-      ocaml_lib "denest";
+      ocaml_lib "mcmc";
       ocaml_lib ~extern:true ~dir:oUnit_dir "oUnit"
   | _ -> ()
 end
