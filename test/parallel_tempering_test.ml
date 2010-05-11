2f8276155cb28e8cf1100af1058f92c9bd08cfd7
Will M. Farr <w-farr@northwestern.edu>
2010-05-11 13:38:13 -0500

    Made constants in parallel_tempering_test.ml more transparent.
    Also added comment about number of chains required to do accurate TI.

--- test/parallel_tempering_test.ml
+++ test/parallel_tempering_test.ml
@@ -3,6 +3,10 @@
 
     mpiexec -n <number-of-processes> ./parallel_tempering_test.{native,byte}
 
+    To get a good sample in TI, you should have at least 5 processes
+    (recall that you are doing a Riemann integral with that number of
+    points).
+
     The posterior being simulated is a 1-D gaussian; each chain stores
     its samples into the file pt_test_<beta>_samples.dat, and also
     prints to stdout the thermodynamic integration calculation of the
@@ -22,7 +26,10 @@ let jump_proposal x = Mcmc.uniform_wrapping (-10.0) 10.0 1.0 x
 let log_jump_prob x y = 0.0
 
 let samples = 
-  Mcmc.pt_mcmc_array ~nskip:11 100000 101 log_likelihood log_prior jump_proposal log_jump_prob 0.0
+  let nskip = 11 and 
+      nsamp = 100000 and 
+      nswap = 101 in
+  Mcmc.pt_mcmc_array ~nskip:nskip nsamp nswap log_likelihood log_prior jump_proposal log_jump_prob 0.0
 
 let _ = 
   let out = open_out ("pt_test_" ^ (string_of_float (Mcmc.pt_beta ())) ^ "_samples.dat") in 
