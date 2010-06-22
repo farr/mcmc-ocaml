b1da0f466bb8bbb9625af20af18e5cdd6089c889
Will M. Farr <w-farr@northwestern.edu>
2010-06-22 18:59:50 -0500

    Expunged MPI stuff.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,12 +1,10 @@
 open Ocamlbuild_plugin
 
 let oUnit_dir = "/Users/farr/Documents/code/oUnit"
-let ocamlmpi_dir = "/Users/farr/Documents/code/ocamlmpi"
 
 let _ = dispatch begin function 
   | After_rules -> 
       ocaml_lib "mcmc";
-      ocaml_lib ~extern:true ~dir:oUnit_dir "oUnit";
-      ocaml_lib ~extern:true ~dir:ocamlmpi_dir "mpi"
+      ocaml_lib ~extern:true ~dir:oUnit_dir "oUnit"
   | _ -> ()
 end
