d486ae299705240fd38bcefcafc2d8cafe4a4e16
Will M. Farr <w-farr@northwestern.edu>
2010-12-08 09:40:36 -0500

    Added jump and jump_prob that stop higher in the interp tree.

--- interpolate_pdf.ml
+++ interpolate_pdf.ml
@@ -29,11 +29,21 @@ module type INTERPOLATE_PDF = sig
   (** Draw a point from the interpolated PDF. *)
   val draw : interp_pdf -> point
 
+  (** Draw a point from an interpolated PDF, but instead of descending
+      the tree completely, stop when the number of points in the cell
+      becomes smaller than the given [n]. *)
+  val draw_high_level : int -> interp_pdf -> point
+
   (** [jump_prob interp_pdf source target] computes the probability
       that the output point of [draw] will be [target].  The [source]
       argument isn't used, but appears for compatibility with the jump
       probability functions expected by [Mcmc]. *)
   val jump_prob : interp_pdf -> 'a -> point -> float
+
+  (** The jump probability for the
+      {Interpolate_pdf.INTERPOLATE_PDF.draw_high_level} jump
+      proposal. *)
+  val jump_prob_high_level : int -> interp_pdf -> 'a -> point -> float
 end
 
 module Make(S : PROB_SPACE) : INTERPOLATE_PDF with type point = S.point = struct
@@ -70,34 +80,65 @@ module Make(S : PROB_SPACE) : INTERPOLATE_PDF with type point = S.point = struct
   let in_tree pt = function 
     | Kdt.Empty -> false
     | Kdt.Cell(_, low, high, _, _) -> 
-        in_bounds pt low high
+      in_bounds pt low high
 
   let rec find_cell pt = function 
     | Kdt.Empty -> raise (Invalid_argument "find_cell: empty tree")
     | Kdt.Cell(_, _, _, Kdt.Empty, Kdt.Empty) as c -> 
-        c
+      c
     | Kdt.Cell(_, _, _, left, right) -> 
-        if in_tree pt left then 
-          find_cell pt left
-        else
-          find_cell pt right
+      if in_tree pt left then 
+        find_cell pt left
+      else
+        find_cell pt right
 
   let make pts low high = 
     (pts, Kdt.tree_of_objects (Array.to_list pts) low high)
 
   let draw (pts, tree) = 
     let pt = pts.(Random.int (Array.length pts)) in 
-    match find_cell (S.coord pt) tree with 
-      | Kdt.Empty -> raise (Failure "draw: empty tree")
-      | Kdt.Cell(_,low,high,_,_) -> 
+      match find_cell (S.coord pt) tree with 
+        | Kdt.Empty -> raise (Failure "draw: empty tree")
+        | Kdt.Cell(_,low,high,_,_) -> 
           S.point (random_in_volume low high)
+
+  let draw_high_level n (pts, tree) = 
+    let pt = pts.(Random.int (Array.length pts)) in 
+    let cds = S.coord pt in
+    let rec draw = function 
+      | Kdt.Empty -> raise (Failure "draw_high_level: encountered empty tree!")
+      | Kdt.Cell(pts, low, high, left, right) -> 
+        if List.length pts <= n then 
+          S.point (random_in_volume low high)
+        else if in_tree cds left then 
+          draw left
+        else
+          draw right in 
+      draw tree
 
   let jump_prob (pts, tree) _ pt = 
     let n = float_of_int (Array.length pts) in 
       match find_cell (S.coord pt) tree with 
         | Kdt.Empty -> raise (Failure "jump_prob: empty tree")
         | Kdt.Cell(objs, low, high, _, _) -> 
-            let nobjs = float_of_int (List.length objs) and 
-                v = Kdt.bounds_volume low high in 
-              nobjs /. (v *. n)
+          let nobjs = float_of_int (List.length objs) and 
+              v = Kdt.bounds_volume low high in 
+            nobjs /. (v *. n)
+
+  let jump_prob_high_level n (pts, tree) _ pt = 
+    let npts = Array.length pts in
+    let cds = S.coord pt in 
+    let rec prob = function 
+      | Kdt.Empty -> raise (Failure "jump_prob_high_level: encountered empty tree!")
+      | Kdt.Cell(pts, low, high, left, right) -> 
+        let ncell = List.length pts in 
+        if ncell <= n then 
+          (* Drew from this cell. *)
+          let v = Kdt.bounds_volume low high in 
+            (float_of_int ncell)/.((float_of_int npts)*.v)
+        else if in_tree cds left then 
+          prob left
+        else
+          prob right in 
+      prob tree
 end
