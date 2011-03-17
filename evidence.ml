7111375aef36d4ad2d893281e4a814e27ebc3534
Will M. Farr <w-farr@northwestern.edu>
2011-03-16 18:33:23 -0700

    Moved to GPL 3.0, added README

--- evidence.ml
+++ evidence.ml
@@ -1,3 +1,19 @@
+(*  evidence.ml: Evidence calculations by various methods.
+    Copyright (C) 2011 Will M. Farr <w-farr@northwestern.edu>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. *)
+
 (** Various algorithms for computing the Bayesian evidence from an
     MCMC sample. *)
 
--- stats.mli
+++ stats.mli
@@ -1,3 +1,19 @@
+(*  stats.mli: Statistical functions.
+    Copyright (C) 2011 Will M. Farr <w-farr@northwestern.edu>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. *)
+
 (** Useful statistics functions. *)
 
 (** [mean xs] returns the mean value. *)
